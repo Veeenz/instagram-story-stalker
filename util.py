923d60dc08ea82d708fc57b466be28e5bb32fbe6
Vincenzo Filetti <vzfiletti@gmail.com>
2018-08-14 10:00:36 +0200

    notify for telegram added

--- util.py
+++ util.py
@@ -3,3 +3,12 @@ def success(msg, data=''):
 
 def fail(msg):
     return {'success': False, 'message': msg}
+
+def notify(msg, chatid):
+    import requests, json
+    with open('config.json', 'r') as target:
+        config = json.loads(target.read())
+    token = config["token"]
+    sendMessageUrl = 'https://api.telegram.org/bot{}/sendMessage?text={}&chat_id={}'
+    req = requests.get(sendMessageUrl.format(token, msg, chatid), timeout=3)
+    return req
