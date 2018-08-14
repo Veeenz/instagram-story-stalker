b316bf0c9fb69cca1eb1d69264ff9db057b6b7f1
Vincenzo Filetti <vzfiletti@gmail.com>
2018-08-14 10:58:14 +0200

    Clean main

--- main.py
+++ main.py
@@ -5,20 +5,10 @@ username = ''
 password = ''
 def getStory(account, userid):
     account.SendRequest('feed/user/' + str(userid) + '/reel_media/')
-#account = InstagramAPI(username, password)
-#account.login()
-"""if account.LastJson['status'] == 'fail':
-    print('Cannot login')
-"""
-account = pickle.load(open('acc','rb'))
-#pickle.dump(account, open('acc','wb'))
-"""
-user = ''
-account.searchUsername(user)
-userid = account.LastJson['user']['pk']
-print(getStory(account, userid))"""
+account = InstagramAPI(username, password)
+account.login()
+
 
 a = Stalker(account)
-a.addPage('vinzfile')
 a.loadAllPages()
 a.startStalking()
