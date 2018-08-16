8ac8e9d12b97a816c7ef1257b76f70d3c7ef2ad8
Vincenzo Filetti <vzfiletti@gmail.com>
2018-08-16 10:43:05 +0200

    remove method on Database class

--- Database.py
+++ Database.py
@@ -9,12 +9,19 @@ class Database:
     def save(self, document, data):
         result = self.db[document].insert_one(data)
         return hasattr(result,'inserted_id')
+    
     def fetch(self, document, key, value, multiple=False):
         if key == '' and value == '' and multiple == True:
             return self.db[document].find()
         if multiple:
             return self.db[document].find({key: value})    
         return self.db[document].find_one({key: value})
+
+    def delete(self, document, key, value):
+        self.db[document].delete_one({key:value})
+
+    def pull(self, document, key, value, field, data):
+        self.db[document].update_One({key:value}, {'$pull': {field: data}})
 
     def append(self, document, key, value, field, data):
         return self.db[document].update_one(
